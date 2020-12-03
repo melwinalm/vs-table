49d288360ad952aaece26c4e18ce866fcbc1506c
melwinalm <melwinalm@gmail.com>
2020-12-03 23:51:52 +0530

    Separated sorting functions - not integrated yet

--- src/components/others/sorting.js
+++ src/components/others/sorting.js
@@ -1,9 +1,21 @@
-const NumericSorting = (data, sortField, sortOrder) => {
+import {SORTING_TYPES} from './enums';
 
+const NumericSorting = (data, sortField, sortOrder) => {
+	if(sortOrder == SORTING_TYPES.ASCENDING){
+		data.sort((a, b) => a[sortField] - b[sortField])
+	}
+	else {
+		data.sort((a, b) => b[sortField] - a[sortField]);
+	}
 }
 
 const StringSorting = (data, sortField, sortOrder) => {
-
+	if(sortOrder == SORTING_TYPES.ASCENDING){
+		data.sort((a, b) => (a[sortField] < b[sortField] ? -1 : 1));
+	}
+	else {
+		data.sort((a, b) => (a[sortField] > b[sortField] ? -1 : 1));
+	}
 }
 
 const DateSorting = (data, sortField, sortOrder) => {
