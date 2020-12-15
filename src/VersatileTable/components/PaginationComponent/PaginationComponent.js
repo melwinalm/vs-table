c78848446467bbf633de0101a221cb455caf66f1
SyedAfrozPasha <afroz.pasha26@gmail.com>
2020-12-15 23:41:05 +0530

    Added styles for pagination component

--- src/VersatileTable/components/PaginationComponent/PaginationComponent.js
+++ src/VersatileTable/components/PaginationComponent/PaginationComponent.js
@@ -1,12 +1,15 @@
 import React from "react";
+import arrowLeft from "../../assets/icons/arrow-left-circle.svg";
+import arrowRight from "../../assets/icons/arrow-right-circle.svg";
+import "./PaginationComponent.scss";
 
 function PaginationComponent({defaultPageSize, currentPage, totalSize, ChangePage }) {
   return (
-		<div>
-			<button onClick={() => ChangePage(-1)} disabled={currentPage <= 1}>Previous</button>|
-			<span>Current Page: {currentPage}</span>|
-			<span>Showing items from {((currentPage-1) * defaultPageSize) + 1} to {Math.min(currentPage * defaultPageSize, totalSize)}</span>|
-			<button onClick={() => ChangePage(1)} disabled={currentPage >= Math.ceil(totalSize/defaultPageSize)}>Next</button>
+		<div className="vt-pagination">
+			<button className="vt-pagination-previous-btn" onClick={() => ChangePage(-1)} disabled={currentPage <= 1}><img className="arrow-left-icon" src={arrowLeft} alt="Previous"/> Previous</button>&nbsp;|&nbsp;
+			<span className="vt-pagination-current-page">Current Page: <strong>{currentPage}</strong></span>&nbsp;|&nbsp;
+			<span className="vt-pagination-total-pages">Showing items from {((currentPage-1) * defaultPageSize) + 1} to {Math.min(currentPage * defaultPageSize, totalSize)}</span>&nbsp;|&nbsp;
+			<button className="vt-pagination-next-btn" onClick={() => ChangePage(1)} disabled={currentPage >= Math.ceil(totalSize/defaultPageSize)}>Next <img className="arrow-right-icon" src={arrowRight} alt="Next"/></button>
 		</div>
 	)
 }
