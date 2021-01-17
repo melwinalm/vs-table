2a988c0790d8c21a4a0a598dee4482723cab5d7b
SyedAfrozPasha <afroz.pasha26@gmail.com>
2021-01-17 13:11:33 +0530

    Add PropType validation and Error Boundary

---
+++ src/VersatileTable/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ErrorBoundary.scss";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    console.error("Error Boundary (error):", error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="vt-error-boundary">
+          Oops! Something went wrong
+        </div>
+      )
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
